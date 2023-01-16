325afe634d703b8bd03e46ee886a1613c8eaa556
Michal Hoftich <michal.h21@gmail.com>
2023-01-16 15:47:45 +0100

    distance calculations

--- src/search.lua
+++ src/search.lua
@@ -94,6 +94,139 @@ local function strip_tags(text)
 end
 
 
+--[[
+    Function: EditDistance
+    Finds the edit distance between two strings or tables. Edit distance is the minimum number of
+    edits needed to transform one string or table into the other.
+    
+    Parameters:
+    
+        s - A *string* or *table*.
+        t - Another *string* or *table* to compare against s.
+        lim - An *optional number* to limit the function to a maximum edit distance. If specified
+            and the function detects that the edit distance is going to be larger than limit, limit
+            is returned immediately.
+            
+    Returns:
+    
+        A *number* specifying the minimum edits it takes to transform s into t or vice versa. Will
+            not return a higher number than lim, if specified.
+            
+    Example:
+        :EditDistance( "Tuesday", "Teusday" ) -- One transposition.
+        :EditDistance( "kitten", "sitting" ) -- Two substitutions and a deletion.
+        returns...
+        :1
+        :3
+            
+    Notes:
+    
+        * Complexity is O( (#t+1) * (#s+1) ) when lim isn't specified.
+        * This function can be used to compare array-like tables as easily as strings.
+        * The algorithm used is Damerau–Levenshtein distance, which calculates edit distance based
+            off number of subsitutions, additions, deletions, and transpositions.
+        * Source code for this function is based off the Wikipedia article for the algorithm
+            <http://en.wikipedia.org/w/index.php?title=Damerau%E2%80%93Levenshtein_distance&oldid=351641537>.
+        * This function is case sensitive when comparing strings.
+        * If this function is being used several times a second, you should be taking advantage of
+            the lim parameter.
+        * Using this function to compare against a dictionary of 250,000 words took about 0.6
+            seconds on my machine for the word "Teusday", around 10 seconds for very poorly 
+            spelled words. Both tests used lim.
+            
+    Revisions:
+        v1.00 - Initial.
+]]
+
+
+local ulen = utf8.len
+local ucodepoint = utf8.codepoint
+local min = math.min
+local max = math.max
+
+-- modified version from this gist: https://gist.github.com/Nayruden/427389
+function edit_distance( s, t, lim )
+    local s_len, t_len = ulen(s), ulen(t) -- Calculate the sizes of the strings or arrays
+    if lim and math.abs( s_len - t_len ) >= lim then -- If sizes differ by lim, we can stop here
+        return lim
+    end
+    
+    -- Convert string arguments to arrays of ints (ASCII values)
+    if type( s ) == "string" then
+        s = { ucodepoint( s, 1, s_len ) }
+    end
+    
+    if type( t ) == "string" then
+        t = { ucodepoint( t, 1, t_len ) }
+    end
+    
+    -- local min = math.min -- Localize for performance
+    local num_columns = t_len + 1 -- We use this a lot
+    
+    local d = {} -- (s_len+1) * (t_len+1) is going to be the size of this array
+    -- This is technically a 2D array, but we're treating it as 1D. Remember that 2D access in the
+    -- form my_2d_array[ i, j ] can be converted to my_1d_array[ i * num_columns + j ], where
+    -- num_columns is the number of columns you had in the 2D array assuming row-major order and
+    -- that row and column indices start at 0 (we're starting at 0).
+    
+    for i=0, s_len do
+        d[ i * num_columns ] = i -- Initialize cost of deletion
+    end
+    for j=0, t_len do
+        d[ j ] = j -- Initialize cost of insertion
+    end
+    
+    for i=1, s_len do
+        local i_pos = i * num_columns
+        local best = lim -- Check to make sure something in this row will be below the limit
+        for j=1, t_len do
+            local add_cost = (s[ i ] ~= t[ j ] and 1 or 0)
+            local val = min(
+                d[ i_pos - num_columns + j ] + 1,                               -- Cost of deletion
+                d[ i_pos + j - 1 ] + 1,                                         -- Cost of insertion
+                d[ i_pos - num_columns + j - 1 ] + add_cost                     -- Cost of substitution, it might not cost anything if it's the same
+            )
+            d[ i_pos + j ] = val
+            
+            -- Is this eligible for tranposition?
+            if i > 1 and j > 1 and s[ i ] == t[ j - 1 ] and s[ i - 1 ] == t[ j ] then
+                d[ i_pos + j ] = min(
+                    val,                                                        -- Current cost
+                    d[ i_pos - num_columns - num_columns + j - 2 ] + add_cost   -- Cost of transposition
+                )
+            end
+            
+            if lim and val < best then
+                best = val
+            end
+        end
+        
+        if lim and best >= lim then
+            return lim
+        end
+    end
+    
+    return d[ #d ]
+end
+
+-- like
+local function calc_distance(sis, alma)
+  -- clean both records first
+  local clean_sis = sis:gsub("ISBN", ""):gsub("[0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-]?[0-9%-]?[0-9%-]?[0-9%-]?", "")
+  clean_sis = clean_sis:gsub("[0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-][0-9%-]", "")
+  -- zkus odstranit vydavatele
+  clean_sis = clean_sis:gsub("[^%.]+:[^%.]-([0-9]+)", "%1")
+  -- remove auhtor year from the first part of citation
+  local clean_alma = alma:gsub("^([^%.]+)", function(author) 
+    return author:gsub("[0-9].*", "") 
+  end)
+  local token_sis = table.concat(tokenize(clean_sis))
+  local token_alma = table.concat(tokenize(clean_alma))
+  local length_diff = ulen(token_sis) - ulen(token_alma)
+  local distance = edit_distance(token_sis, token_alma) 
+  print(token_sis, token_alma)
+  return(distance / ulen(token_sis) )
+end
 
 -- add_document(1, "nazdar světe, jak se máš?")
 -- add_document(2, "ahoj světe, co děláš?")
@@ -107,12 +240,14 @@ end
 
 
 
-
+-- calc_distance(original, "Blažek, Bohuslav, 1942-2004. Tváří v tvář obrazovce /. 1995")
 return {
   tokenize = tokenize,
   add_document = add_document,
   search = search,
-  strip_tags = strip_tags
+  strip_tags = strip_tags,
+  edit_distance = edit_distance,
+  calc_distance = calc_distance
 }
 
 
